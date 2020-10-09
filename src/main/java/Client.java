462e06ac125056080ed96fe862099a31932f2707
Thaehan <19021236@vnu.edu.vn>
2020-10-09 19:50:37 +0700

    Design
    New Design

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,13 +1,5 @@
-import javax.swing.AbstractAction;
-import javax.swing.DefaultListModel;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
-import javax.swing.JTextPane;
-import javax.swing.ListSelectionModel;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
@@ -19,13 +11,16 @@ import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import javax.swing.border.BevelBorder;
+import java.awt.event.*;
+
 
 public class Client extends UI {
     private JPanel panel;
     private JTextPane textPane;
     private JTextField textField;
     private JScrollPane scrollPane;
-    private javax.swing.JScrollPane jListScrollPane;
+    private JScrollPane jListScrollPane;
     private javax.swing.JList<String> jList;
 
     private String currentWord;
@@ -40,6 +35,101 @@ public class Client extends UI {
     }
 
     public final void initUI() {
+        setTitle("OXFORD");
+        setSize(800,600);
+
+        JPanel basic = new JPanel();
+        basic.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+        basic.setLayout(new BoxLayout(basic, BoxLayout.PAGE_AXIS));
+
+        JMenuBar menubar = new JMenuBar();
+        JMenu viewMenu = new JMenu("TRANSLATE");
+        JCheckBoxMenuItem showStatusBar = new JCheckBoxMenuItem("Translate then search");
+        showStatusBar.setDisplayedMnemonicIndex(5);
+        showStatusBar.setSelected(false);
+        showStatusBar.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                isTranslated = false;
+            }
+        });
+        viewMenu.add(showStatusBar);
+        menubar.add(viewMenu);
+        setJMenuBar(menubar);
+//
+        JPanel top = new JPanel();
+        top.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+        top.setLayout(new BoxLayout(top, BoxLayout.LINE_AXIS));
+
+        textField = new JTextField();
+        textField.setFont(new java.awt.Font("Tahoma", 0, 20));
+        textField.setPreferredSize(new Dimension(getWidth()/4, getHeight()/9));
+        textField.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                searchKeyReleased(evt);
+            }
+        });
+        JPanel topRight = new JPanel();
+        topRight.setAlignmentX(CENTER_ALIGNMENT);
+        topRight.setLayout(new GridLayout(2, 3, -1, -1));
+
+        JButton search = new JButton("", new ImageIcon("src/main/resources/image/search.png"));
+        JButton add = new JButton("", new ImageIcon("src/main/resources/image/add.png"));
+        JButton delete = new JButton("", new ImageIcon("src/main/resources/image/delete.png"));
+        JButton playSound = new JButton("", new ImageIcon("src/main/resources/image/volume.png"));
+        delete.setPreferredSize(new Dimension(getWidth() * 3 / 16, getHeight()*2/27));
+        delete.addActionListener(new DictionaryDeleteEntry());
+        search.setPreferredSize(new Dimension(getWidth() * 3 / 16, getHeight()*2/27));
+        search.setMnemonic(KeyEvent.VK_ENTER);
+        search.addActionListener(new DictionarySearch());
+        add.setPreferredSize(new Dimension(getWidth() * 3 / 16, getHeight()*2/27));
+        add.addActionListener(new DictionaryAdd());
+        playSound.setPreferredSize(new Dimension(getWidth() * 3 / 17, getHeight()*2/27));
+        playSound.addActionListener(new loadSound());
+
+        JLabel lsearch = new JLabel("               Search");
+        lsearch.setPreferredSize(new Dimension(getWidth() * 3 / 16, getHeight()/27));
+        JLabel ladd = new JLabel("             Add new word");
+        ladd.setPreferredSize(new Dimension(getWidth() * 3 / 16, getHeight()/27));
+        JLabel ldelete = new JLabel("             Delete word");
+        ldelete.setPreferredSize(new Dimension(getWidth() * 3 / 16, getHeight()/27));
+        JLabel lsound = new JLabel("                  Spell");
+        lsound.setPreferredSize(new Dimension(getWidth() * 3 / 16, getHeight()/27));
+        topRight.add(lsearch);
+        topRight.add(ladd);
+        topRight.add(ldelete);
+        topRight.add(lsound);
+        topRight.add(search);
+        topRight.add(add);
+        topRight.add(delete);
+        topRight.add(playSound);
+        top.add(textField);
+        top.add(topRight);
+//
+        JPanel bottom = new JPanel();
+        bottom.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+        bottom.setLayout(new BoxLayout(bottom, BoxLayout.LINE_AXIS));
+
+        jList = new JList<>();
+        jListScrollPane = new JScrollPane();
+        jListScrollPane.setPreferredSize(new Dimension(getWidth()/4, getHeight() *6/7));
+        jList.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jList.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        jListScrollPane.setViewportView(jList);
+        textPane = new JTextPane();
+        textPane.setContentType("text/html");
+        textPane.setEditable(false);
+        scrollPane = new JScrollPane(textPane);
+        scrollPane.setPreferredSize(new Dimension(getWidth()*3/4, getHeight()*8/9));
+        bottom.add(jListScrollPane);
+        bottom.add(scrollPane);
+//
+        basic.add(top);
+        basic.add(bottom);
+        add(basic);
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
     }
 
     private class DictionarySearch extends AbstractAction {
@@ -103,7 +193,6 @@ public class Client extends UI {
                 if (j == n - 1)
                     filteredItems.addElement(item);
             }
-
         }
 
         jListModel = filteredItems;
